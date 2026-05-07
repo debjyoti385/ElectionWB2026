c55b30c6d1e3252f7db5e1daf76b9b9aecd37fd4
Debjyoti Paul <debjyotipaul385@gmail.com>
2026-05-07 02:46:04 -0700

    fix: alphabetical district order in All Seats filter and Districts tab

--- js/app.js
+++ js/app.js
@@ -618,11 +618,11 @@ window.DistrictView = (() => {
   };
   // Ordered canonical district list
   const DISTRICT_ORDER = [
-    'Cooch Behar','Alipurduar','Jalpaiguri','Kalimpong','Darjeeling',
-    'Uttar Dinajpur','Dakshin Dinajpur','Malda','Murshidabad','Nadia',
-    'North 24 Parganas','South 24 Parganas','Kolkata','Howrah','Hooghly',
-    'Purba Medinipur','Paschim Medinipur','Jhargram','Purulia','Bankura',
-    'Purba Bardhaman','Paschim Bardhaman','Birbhum'
+    'Alipurduar','Bankura','Birbhum','Cooch Behar','Dakshin Dinajpur',
+    'Darjeeling','Hooghly','Howrah','Jalpaiguri','Jhargram','Kalimpong',
+    'Kolkata','Malda','Murshidabad','Nadia','North 24 Parganas',
+    'Paschim Bardhaman','Paschim Medinipur','Purba Bardhaman','Purba Medinipur',
+    'Purulia','South 24 Parganas','Uttar Dinajpur'
   ];
 
   let _byDist = {};
